3fc2c69addea5e17bdcf0f943f5242aa6d0699a5
fujis <ppdx999@gmail.com>
2025-03-22 15:00:03 +0900

    chapter 15.2

--- c/vm.c
+++ c/vm.c
@@ -5,23 +5,35 @@
 
 VM vm;
 
+static void resetStack() {
+    vm.stackTop = vm.stack;
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
 	for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+        printf( "          ");
+        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
         disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
-                printValue(constant);
-                printf("\n");
+                push(constant);
                 break;
             }
             case OP_RETURN:
+                printValue(pop());
+                printf("\n");
                 return INTERPRET_OK;
         }
     }
@@ -30,10 +42,22 @@ static InterpretResult run() {
 }
 
 void initVM() {
+    resetStack();
 }
 
 void freeVM() {
 }
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
 
 InterpretResult interpret(Chunk* chunk) {
 	vm.chunk = chunk;
