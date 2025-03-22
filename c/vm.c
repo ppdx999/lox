1e3937e6f00fa884d6ff1614c9b528d30ef0ca34
fujis <ppdx999@gmail.com>
2025-03-22 15:59:41 +0900

    chapter 15

--- c/vm.c
+++ c/vm.c
@@ -12,6 +12,12 @@ static void resetStack() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false)
 
 	for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -31,6 +37,10 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD: BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE: BINARY_OP(/); break;
             case OP_NEGATE: {
                 push(-pop());
                 break;
@@ -43,6 +53,7 @@ static InterpretResult run() {
     }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 void initVM() {
