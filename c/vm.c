a3b5f128c8df8f68fd46ef5043dfc74881699cf8
fujis <ppdx999@gmail.com>
2025-03-23 23:21:12 +0900

    implement chapter 17

--- c/vm.c
+++ c/vm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "debug.h"
+#include "chunk.h"
 #include "common.h"
 #include "compiler.h"
 #include "vm.h"
@@ -76,6 +77,19 @@ Value pop() {
 
 
 InterpretResult interpret(const char* source) {
-    compile(source);
-    return INTERPRET_OK;
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
